ec51f695b504b7dcc8ec8f84ce90ba451d495235
siltif <utkucevik1515@gmail.com>
2025-09-05 17:29:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const http = require("http");
 const express = require("express");
 const { WebSocketServer } = require("ws");
@@ -6,22 +5,16 @@ const { WebSocketServer } = require("ws");
 const app = express();
 const server = http.createServer(app);
 
-// Statik dosyaları (index.html vs.) sunmak için
 app.use(express.static("public"));
 
-// Render ortamında PORT değişkeni gelir, yoksa 3000 kullan
 const PORT = process.env.PORT || 3000;
-
-// WebSocket sunucusu
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
   console.log("Yeni kullanıcı bağlandı");
 
   ws.on("message", (msg) => {
-    console.log("Mesaj:", msg.toString());
-
-    // Mesajı tüm bağlı client'lara gönder
+    // Tüm mesajları (chat + WebRTC sinyalleri) diğer client'lara gönder
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === ws.OPEN) {
         client.send(msg.toString());
