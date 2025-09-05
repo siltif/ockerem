b2bb26074ee747e2c854f69780e0958f124f4972
siltif <utkucevik1515@gmail.com>
2025-09-05 17:49:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,28 +4,42 @@ const { WebSocketServer } = require("ws");
 
 const app = express();
 const server = http.createServer(app);
-
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 const wss = new WebSocketServer({ server });
 
-wss.on("connection", (ws) => {
-  console.log("Yeni kullanıcı bağlandı");
+let users = [];
 
+wss.on("connection", (ws) => {
   ws.on("message", (msg) => {
-    // Tüm mesajları (chat + WebRTC sinyalleri) diğer client'lara gönder
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === ws.OPEN) {
-        client.send(msg.toString());
-      }
-    });
+    const data = JSON.parse(msg);
+
+    if (data.type === "join") {
+      ws.username = data.username;
+      ws.photo = data.photo;
+      users = [...wss.clients]
+        .filter(c => c.readyState === ws.OPEN && c.username)
+        .map(c => ({ username: c.username, photo: c.photo }));
+      broadcast({ type: "join", users });
+    } else {
+      broadcast(data, ws);
+    }
   });
 
   ws.on("close", () => {
-    console.log("Kullanıcı ayrıldı");
+    users = users.filter(u => u.username !== ws.username);
+    broadcast({ type: "join", users });
   });
 });
+
+function broadcast(msg, exclude) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === client.OPEN && client !== exclude) {
+      client.send(JSON.stringify(msg));
+    }
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`✅ Server çalışıyor: http://localhost:${PORT}`);
